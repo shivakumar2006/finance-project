fc1c17429cde63ff59afa33dc58709d5fe74d91f
shivakumar2006 <official.shivakumar06@gmail.com>
2026-04-04 17:58:00 +0530

    commit

---
+++ frontend/src/redux/api/api.js
@@ -0,0 +1,120 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const Api = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:8080/api/v1",
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().auth.token;
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+            headers.set("Content-Type", "application/json");
+            return headers;
+        },
+    }),
+    tagTypes: ["Auth", "User", "Transaction", "Dashboard"],
+    endpoints: (builder) => ({
+        // auth
+        register: builder.mutation({
+            query: (data) => ({
+                url: "/auth/register",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["Auth"]
+        }),
+
+        login: builder.mutation({
+            query: (data) => ({
+                url: "/auth/login",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["Auth"]
+        }),
+
+        // users 
+        getUsers: builder.query({
+            query: () => "/users",
+            providesTags: ["User"],
+        }),
+
+        getUserById: builder.query({
+            query: (id) => `/users/${id}`,
+            providesTags: ["User"]
+        }),
+
+        updateUser: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/users/${id}`,
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ["User"]
+        }),
+
+        deleteUser: builder.mutation({
+            query: (id) => ({
+                url: `/users/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["User"]
+        }),
+
+        // tansactions 
+        getTransaction: builder.query({
+            query: () => "/transactions",
+            providesTags: ["Transaction"]
+        }),
+
+        getTransactionById: builder.query({
+            query: (id) => `/transactions/${id}`,
+            providesTags: ["Transaction"]
+        }),
+
+        createTransaction: builder.mutation({
+            query: (data) => ({
+                url: "/transactions",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["Transaction"]
+        }),
+
+        updateTransaction: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/transactions/${id}`,
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ["Transaction"]
+        }),
+
+        deleteTransaction: builder.mutation({
+            query: (id) => ({
+                url: `/transaction/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Transaction"]
+        }),
+
+        // dashboard 
+        getSummary: builder.query({
+            query: () => "/dashboard",
+            providesTags: ["Dashboard"]
+        }),
+
+        getTrends: builder.query({
+            query: () => "/dashboard/trends",
+            providesTags: ["Dashboard"]
+        }),
+
+        getCategoryTotals: builder.query({
+            query: () => "/dashboard/categories",
+            providesTags: ["Dashboard"]
+        }),
+    })
+})
+
+export const { useRegisterMutation, useLoginMutation, useGetUsersQuery, useGetUserByIdQuery, useUpdateUserMutation, useDeleteUserMutation, useGetTransactionQuery, useGetTransactionByIdQuery, useCreateTransactionMutation, useUpdateTransactionMutation, useDeleteTransactionMutation, useGetSummaryQuery, useGetTrendsQuery, useGetCategoryTotalsQuery } = Api;
